801430e5e38556b78d9571f3ffe856eff4a4eb6c
Nate Lane <nate@sidekicklouisville.com>
2019-01-28 20:33:45 -0500

    1-28-2019

---
+++ js/chungus.js
@@ -0,0 +1,37 @@
+var chungusStudents = [
+  {
+  name: 'Big Chungus',
+  age: 1000,
+  grade: 89.5,
+  track: 'Honors',
+  gender: 'M'
+  },
+  {
+  name: 'Bigger Chungus',
+  age: 1000,
+  grade: 89.5,
+  track: 'Honors',
+  gender: 'M'
+  },
+  {
+  name: 'Biggest Chungus',
+  age: 1000,
+  grade: 89.5,
+  track: 'Honors',
+  gender: 'M'
+  },
+  {
+  name: 'Infinite Chungus',
+  age: 1000,
+  grade: 89.5,
+  track: 'Honors',
+  gender: 'M'
+  },
+  {
+  name: 'Baby Chungus',
+  age: 1000,
+  grade: 89.5,
+  track: 'Honors',
+  gender: 'M'
+  },
+];
